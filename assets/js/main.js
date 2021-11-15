6d0f132507276b14eed1f3ae20e2d3168e44a3c0
Felipe Feitosa <pro.felipefeitosa@gmail.com>
2021-11-15 10:17:26 -0300

    returnDigit function has been created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,17 @@
 //creating immediate function
 (function(){
 
+//querying elements 
+const inputCPF = document.querySelector("input");
+const button = document.querySelector("button");
+
+//adding listeners 
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const stringNumbers = inputCPF.value.replace(/\D+/g, "");
+  
+})
+
+
 })()
